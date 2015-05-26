9f941dad180d359ca715511e6bfd199e7d8f74a8
punchdrunker <takafumi.nanao@mixi.co.jp>
2015-05-26 15:27:53 +0900

    init

---
+++ Hello/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hello
+//
+//  Created by takafumi.nanao on 2015/05/26.
+//  Copyright (c) 2015年 punchdrunker.org. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
